63c9e9c931485c053e44b9a5a8d88a034629926f
Dhanesh <d_budhrani90@hotmail.com>
2016-10-26 14:58:20 +0200

    Floating boat

---
+++ Assets/Scripts/Float.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour {
+
+	public float waterLevel = 0f;
+	public float floatHeight = 2f;
+	public float bounceDamp = 0.05f;
+	public Vector3 buoyancyCenterOffset;
+
+	private float forceFactor;
+	private Vector3 actionPoint;
+	private Vector3 uplift;
+	 	  	 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		actionPoint = transform.position + transform.TransformDirection(buoyancyCenterOffset);
+		forceFactor = 1f - ((actionPoint.y - waterLevel)/floatHeight);
+
+		if (forceFactor > 0f) {
+			uplift = -Physics.gravity * (forceFactor - GetComponent<Rigidbody>().velocity.y * bounceDamp);
+			GetComponent<Rigidbody>().AddForceAtPosition(uplift, actionPoint);
+		}
+	}
+}
