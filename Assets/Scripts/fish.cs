e29e48378604413a0c6199b94e86d4b26f93d61e
MengweiAmy <sunmengwei@amyvivi.eduroam.wireless.dtu.dk>
2016-11-02 15:43:15 +0100

    Add fish objects(two kinds of fish)

---
+++ Assets/Scripts/fish.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fish : MonoBehaviour {
+
+	public GameObject fishPrefabs;
+
+	static int fishnum=5;
+
+	public static GameObject[] allFish = new GameObject[fishnum];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
