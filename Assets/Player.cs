7317103987379b0c563ef6baefac318dc2f87be6
kostantin <kostandin.thana@gmail.com>
2016-11-09 11:40:13 +0100

    Score and Win Screen
    It doesn't compile due to erros but the scipts and folders in canvas and
    the connection of the score to Player script are there.

---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour {
+
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Player = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        void OnTriggerEnter(Collider other)
+{
+            if (other.gameObject.CompareTag("Pick Up"))
+            {
+                other.gameObject.SetActive(false);
+                count = count + 1;
+                SetCountText();
+
+            }
+
+
+        }
+        void SetCountText ()
+{
+            count Text.text = "count: " + count.ToString();
+            if (count >= 10)
+                winText.text = "You Win !";
+        }
+    }
+}
