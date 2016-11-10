15d473d820ce0203fa572da1155c1f3a6da02148
MengweiAmy <sunmengwei@amyvivi.eduroam.wireless.dtu.dk>
2016-11-10 16:47:03 +0100

    Made fish animation work
    Add fishMovement script; Add animator controller;Deleted fish script

---
+++ Assets/Scripts/fishMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishMovement : MonoBehaviour {
+	
+	public float speed = 6f;
+
+	Vector3 posi;
+	Animator anim;
+	Rigidbody fishRigid;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+		fishRigid = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()//call on every physics update
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Animating (h, v);
+	}
+
+	void Move(float h, float v)
+	{
+		posi.Set(h, 0f, v);
+		posi = posi.normalized * speed * Time.deltaTime;
+
+		fishRigid.MovePosition (transform.position+posi);
+	}
+
+	void Animating(float h, float v) 
+	{
+		//bool isHit = h != 0f || v != 0f;
+		bool isHit = false;
+		anim.SetBool ("isHit", isHit);
+	}
+
+
+}
