8117f65bb75e5ae6203f2e4c3afdc6f4dfe853bd
Dhanesh <d_budhrani90@hotmail.com>
2016-11-16 11:09:46 +0100

    Boat moves with arrows

---
+++ Assets/MoveBoat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBoat : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.Translate(Vector3.back * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+
+	}
+}
