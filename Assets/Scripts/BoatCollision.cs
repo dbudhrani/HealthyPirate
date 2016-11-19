a6c17891bd77bc8ad3345ea5f7d6ae92874d5b7f
Dhanesh <d_budhrani90@hotmail.com>
2016-11-19 16:52:48 +0100

    We destroy only the clones

--- Assets/Scripts/BoatCollision.cs
+++ Assets/Scripts/BoatCollision.cs
@@ -13,14 +13,17 @@ public class BoatCollision : MonoBehaviour {
 		Debug.Log("Object name = " + col.gameObject.name);
 		if (col.gameObject.name == "Stone(Clone)" || col.gameObject.name == "Stone") {
 			Debug.Log("Damn, I crashed");
+			Destroy(col.gameObject);
 			ms.incrementCollidedNumberStones();
 		}
 		if (col.gameObject.name == "cruscarp(Clone)" || col.gameObject.name == "cruscarp") {
 			Debug.Log("Genius, I got a fish!");
+			Destroy(col.gameObject);
 			ms.incrementCapturedNumberFishes();
 		}
 		if (col.gameObject.name == "Tree(Clone)" || col.gameObject.name == "Tree") {
 			Debug.Log("I crashed with a tree!");
+			Destroy(col.gameObject);
 			ms.incrementCollidedNumberTrees();
 		}
 	}
