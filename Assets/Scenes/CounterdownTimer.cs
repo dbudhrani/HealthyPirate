e70cf2ef20f33b195ecd4dcf9a1243f4599a8acd
kostantin <kostandin.thana@gmail.com>
2016-11-23 10:41:33 +0100

    fish and time count

---
+++ Assets/Scenes/CounterdownTimer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour {
+
+    public float Seconds = 59;
+    public float Minutes = 0;
+
+    void Update()
+    {
+        if (Seconds <= 0) 
+        {
+            Seconds = 59;
+            if (Minutes >= 1) 
+            {
+                Minutes--;
+            }
+            else
+            {
+                Minutes = 0;
+                Seconds = 0;
+            }
+        }
+        else
+        {
+            Seconds = Time.deltaTime;
+        }
+      }
+
+}
+
+
+
+
+
