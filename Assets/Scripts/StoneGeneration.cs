6b6974727667f2259d4dad37a606290b52e82295
Thang <pandothang@gmail.com>
2016-11-23 17:24:40 +0100

    Implemented green bar moving

--- Assets/Scripts/StoneGeneration.cs
+++ Assets/Scripts/StoneGeneration.cs
@@ -8,8 +8,8 @@ public class StoneGeneration : MonoBehaviour {
 	public float maxX = 3.0f;
 	public float minZ = -3.0f;
 	public float maxZ = 3.0f;
-	public float minWaitTime = 2.0f;
-	public float maxWaitTime = 4.0f;
+	public float minWaitTime = 3.0f;
+	public float maxWaitTime = 3.0f;
 	public float minScale = 0.005f;
 	public float maxScale = 0.01f;
 
@@ -17,11 +17,16 @@ public class StoneGeneration : MonoBehaviour {
 	private int counter;
 	private MainScript ms;
 
-	void Start () {
+    public Queue stoneQueue;
+
+    void Start () {
 		levelCompleted = false;
 		ms = GetComponent<MainScript>();
-		StartCoroutine(generateRandomStone());
-	}
+        stoneQueue = new Queue();
+        StartCoroutine(generateRandomStone());
+        
+
+    }
 	
 	void Update () {
 		Debug.Log("StoneGeneration");
@@ -34,9 +39,21 @@ public class StoneGeneration : MonoBehaviour {
 			Vector3 position = new Vector3(Random.Range(minX, maxX), 0.0f, Random.Range(minZ, maxZ));
 			//Debug.Log(position);
 			GameObject newStone = (GameObject) Instantiate(prefab, position, Quaternion.identity);
+
+            stoneQueue.Enqueue(newStone); //adding stones to the queue
+
 			float stoneScale = Random.Range(minScale, maxScale);
 			newStone.transform.localScale = new Vector3(stoneScale, stoneScale, stoneScale);
 			ms.incrementTotalNumberStones();
 		}
 	}
+
+    void generateStone()
+    {
+        Vector3 position = new Vector3(Random.Range(minX, maxX), 0.0f, Random.Range(minZ, maxZ));
+        //Debug.Log(position);
+        GameObject newStone = (GameObject)Instantiate(prefab, position, Quaternion.identity);
+
+        stoneQueue.Enqueue(newStone); //adding stones to the queue
+    }
 }
