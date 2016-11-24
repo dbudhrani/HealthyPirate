01b80f46b6bb7132cd7f575d21d10c0ab66f6469
kostantin <kostandin.thana@gmail.com>
2016-11-24 13:29:27 +0100

    finished counter and fish count

---
+++ Assets/myTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class myTimer : MonoBehaviour {
+
+    public float playerTimer = 99;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerTimer -= Time.deltaTime;
+        timerText.text = playerTimer.ToString("f0");
+        print(playerTimer);
+	}
+}
---
+++ Assets/score.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    public int fishValue;
+    public Text scoreText;
+    private MainScript ms;
+
+
+	// Use this for initialization
+	void Start () {
+        fishValue = 0;
+        ms = GameObject.Find("Brain").GetComponent<MainScript>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = ms.getCapturedNumberFishes().ToString();
+    
+
+    }
+
+   /* void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "cruscarp(Clone)")
+        {
+            fishValue = fishValue = 1;
+        }
+    }*/
+}
